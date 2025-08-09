1c7db0eb4eb7848c8e2aa59f124bfa7defa2968c
Lobato Code <lobatocode@gmail.com>
2025-08-09 12:24:58 -0400

    Add files via upload

---
+++ variaveis.py
@@ -0,0 +1,74 @@
+nome_do_canal = "Lobato Code"
+
+print(nome_do_canal)
+
+##########################################
+
+# guardamos o numero 25 na variavel idade
+idade = 25
+
+# com o print mostraremos na tela o que guardamos na variavel
+print(idade)
+
+##########################################
+
+# juntando o texto com a variavel "nome_do_canal"
+print("O nome do canal é", nome_do_canal)
+
+##########################################
+
+# String: sequencia de caracteres. Entre aspas smiples ou duplas.
+cidade = "Manaus"
+frase_favorita = 'Python é muito legal'
+
+print(cidade)
+print(frase_favorita)
+
+##########################################
+
+# Integer (ou int): numeros inteiros. Sem aspas.
+ano_atual = 2025
+ano_do_seu_nascimento = 1995
+gols_na_partida = 3
+temperatura = -10
+
+# Podemos fazer contas com eles.
+sua_idade = ano_atual - ano_do_seu_nascimento
+print("Voce tem", sua_idade, "anos")
+
+##########################################
+
+# Float: numeros com parte decimal. O separador é o ponto.
+preco_do_acai = 12.50
+altura_em_metros = 1.81
+
+print("O acai custa R$", preco_do_acai)
+print("Sua altura é", altura_em_metros, "metros")
+
+##########################################
+
+# Boolean: True ou False. Primeira letra sempre maiuscula. Sem aspas.
+esta_chovendo = False
+voce_e_maior_de_idade = True
+
+print("Esta chovendo agora?", esta_chovendo)
+print("Voce e maior de idade?", voce_e_maior_de_idade)
+
+##########################################
+
+aluno = "Maria"
+idade_do_aluno = 21
+nota_media = 8.7
+esta_aprovado = True
+
+print("--- Ficha do Aluno(a) ---")
+print("Nome:",aluno)
+print("Idade:",idade_do_aluno)
+print("Nota Média:",nota_media)
+print("Está aprovado?",esta_aprovado)
+
+##########################################
+
+# type(): mostra o tipo da variavel
+print(type(aluno)) # <class 'str'> = String
+
